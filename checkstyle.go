0044b392dff58de80cc3c3f8fc7042368b5985df
Jesse Donat <donatj@gmail.com>
2019-01-16 13:00:41 -0600

    Roughs in checkstyle structure

---
+++ checkstyle.go
@@ -0,0 +1,61 @@
+package checkstyle
+
+import (
+	"encoding/xml"
+	"io"
+
+	"golang.org/x/net/html/charset"
+)
+
+// Defined severity levels from http://checkstyle.sourceforge.net/property_types.html#severity
+type SeverityLevel string
+
+const (
+	SeverityIgnore  SeverityLevel = "ignore"
+	SeverityInfo    SeverityLevel = "info"
+	SeverityWarning SeverityLevel = "warning"
+	SeverityError   SeverityLevel = "error"
+)
+
+type Checkstyle struct {
+	XMLName xml.Name        `xml:"checkstyle"`
+	Version string          `xml:"version,attr"`
+	File    CheckstyleFiles `xml:"file"`
+}
+
+type CheckstyleFile struct {
+	Name  string                `xml:"name,attr"`
+	Error []CheckstyleFileError `xml:"error"`
+}
+
+type CheckstyleFileError struct {
+	Line     int           `xml:"line,attr"`
+	Severity SeverityLevel `xml:"severity,attr"`
+	Message  string        `xml:"message,attr"`
+	Source   string        `xml:"source,attr"`
+}
+
+type CheckstyleFiles []CheckstyleFile
+
+func (chk CheckstyleFiles) FromName(name string) *CheckstyleFile {
+	for _, c := range chk {
+		if c.Name == name {
+			return &c
+		}
+	}
+
+	return nil
+}
+
+func Decode(r io.Reader) (*Checkstyle, error) {
+	dec := xml.NewDecoder(r)
+	dec.CharsetReader = charset.NewReaderLabel
+
+	chk := Checkstyle{}
+	err := dec.Decode(&chk)
+	if err != nil {
+		return nil, err
+	}
+
+	return &chk, nil
+}
---
+++ diff.go
@@ -0,0 +1,141 @@
+package checkstyle
+
+import (
+	"regexp"
+)
+
+type DiffOptions struct {
+	MaxLineDiff int
+}
+
+func Diff(left, right *Checkstyle, opt DiffOptions) (*Checkstyle, *Checkstyle) {
+	lout := &Checkstyle{}
+	rout := &Checkstyle{}
+
+	names := allNames(left, right)
+	for n, s := range names {
+
+		if s == sideLeft {
+			lout.File = append(lout.File, *left.File.FromName(n))
+			continue
+		}
+
+		if s == sideRight {
+			rout.File = append(rout.File, *right.File.FromName(n))
+			continue
+		}
+
+		lf := left.File.FromName(n).Error
+		rf := right.File.FromName(n).Error
+
+		// running through zero first prevernts accidental lose equality where
+		// actual equality exists several lines below
+		lf, rf = fileErrorWoExactEq(lf, rf, 0)
+		if opt.MaxLineDiff > 0 {
+			lf, rf = fileErrorWoExactEq(lf, rf, opt.MaxLineDiff)
+		}
+
+		if len(lf) > 0 {
+			lout.File = append(lout.File, CheckstyleFile{
+				Name:  n,
+				Error: lf,
+			})
+		}
+
+		if len(rf) > 0 {
+			rout.File = append(rout.File, CheckstyleFile{
+				Name:  n,
+				Error: rf,
+			})
+		}
+	}
+
+	return lout, rout
+}
+
+type side int
+
+const (
+	sideLeft side = iota + 1
+	sideRight
+	sideBoth
+)
+
+func fileErrorWoExactEq(left, right []CheckstyleFileError, maxLineDiff int) ([]CheckstyleFileError, []CheckstyleFileError) {
+	lout := []CheckstyleFileError{}
+	rout := []CheckstyleFileError{}
+
+leftloop:
+	for _, l := range left {
+		for _, r := range right {
+			if fileErrorEq(l, r, maxLineDiff) {
+				continue leftloop
+			}
+		}
+
+		lout = append(lout, l)
+	}
+
+rightloop:
+	for _, r := range right {
+		for _, l := range left {
+			if fileErrorEq(l, r, maxLineDiff) {
+				continue rightloop
+			}
+		}
+
+		rout = append(rout, r)
+	}
+
+	return lout, rout
+}
+
+func fileErrorEq(lfile, rfile CheckstyleFileError, maxLineDiff int) bool {
+	if lfile.Severity != rfile.Severity || lfile.Source != rfile.Source || !msgEq(lfile.Message, rfile.Message) {
+		return false
+	}
+
+	if abs(lfile.Line-rfile.Line) <= maxLineDiff {
+		return true
+	}
+
+	return false
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+
+	return x
+}
+
+var msgClean = regexp.MustCompile(`[^\p{L}?:().]+`)
+
+// This is an initial attempt at this and should be reworked later to concider
+// things like the line number difference in parts of this that perhaps include
+// line numbers
+func msgEq(left, right string) bool {
+	left = msgClean.ReplaceAllString(left, "|")
+	right = msgClean.ReplaceAllString(right, "|")
+
+	return left == right
+}
+
+func allNames(left, right *Checkstyle) map[string]side {
+	ret := map[string]side{}
+
+	for _, c := range left.File {
+		ret[c.Name] = sideLeft
+	}
+
+	for _, c := range right.File {
+		if _, ok := ret[c.Name]; ok {
+			ret[c.Name] = sideBoth
+		} else {
+			ret[c.Name] = sideRight
+		}
+	}
+
+	return ret
+}
